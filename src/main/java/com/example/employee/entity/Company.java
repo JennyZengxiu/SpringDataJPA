81a13c2f5ab0b8a6b2bee72dd7131ab14b34cfb6
zx <1374040664@qq.com>
2018-06-29 21:07:35 +0800

    add entity

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,47 @@
 package com.example.employee.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Company {
+    @Id
+    Integer id;
+    @Column
+    String companyName;
+    @Column
+    Integer employeesNumber;
+
+    public Company(Integer id,String companyName,Integer employeesNumber){
+        this.id = id;
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setEmployeesNumber(Integer employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public Integer getEmployeesNumber() {
+        return employeesNumber;
+    }
+
 
 }
